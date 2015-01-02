4855bffd1c525bf44384e5c4d3fe977410ea15b5
sudip <soodip.kafle@gmail.com>
2015-01-02 10:23:34 +0545

    Test 33 set vs list for membership test with 'in'

---
+++ tests/test33/t1.py
@@ -0,0 +1,6 @@
+def a():
+    my_list = [1, 2, 3, 5, 12, 11, 16, 14]
+    for i in range(20):
+        if i in my_list:
+            pass
+    return my_list
---
+++ tests/test33/t2.py
@@ -0,0 +1,6 @@
+def a():
+    my_set = {1, 2, 3, 5, 12, 11, 16, 14}
+    for i in range(20):
+        if i in my_set:
+            pass
+    return my_set
