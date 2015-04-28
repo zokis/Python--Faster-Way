9c741019930d34d4822bdf6354716063794ae8af
Marcelo Fonseca Tambalo <marcelo.zokis@gmail.com>
2015-04-28 16:22:05 -0300

    new gen_page.py

--- gen_page.py
+++ gen_page.py
@@ -7,7 +7,6 @@ import timeit
 from jinja2 import Template
 from unipath import Path, DIRS, FILES
 
-TESTS = []
 NUMBER = 1000000
 
 base_path = Path(os.path.abspath(__file__)).parent
@@ -18,56 +17,55 @@ tmp = Path('/tmp')
 
 
 def main():
+    def gen_tests():
+        for Tx, group_x_dir in enumerate(test_path.walk(filter=DIRS), 1):
+            print "Gen Test: %s" % Tx
+            group = []
+            for tx, test_x_file in enumerate(group_x_dir.walk(filter=FILES), 1):
+                print "    Sub Test: %s" % tx
+                file_content = test_x_file.read_file()
 
-    for group_x_dir in test_path.walk(filter=DIRS):
-        group = []
-        TESTS.append(group)
-        for test_x_file in group_x_dir.walk(filter=FILES):
-            file_content = test_x_file.read_file()
+                dis_test_content = dis_template % file_content
 
-            dis_test_content = dis_template % file_content
+                dis_test_file = tmp.child('__pfw_%s_%s' % (group_x_dir.name, test_x_file.name))
+                dis_test_file.write_file(dis_test_content)
 
-            dis_test_file = tmp.child('__pfw_%s_%s' % (group_x_dir.name, test_x_file.name))
-            dis_test_file.write_file(dis_test_content)
+                p = subprocess.Popen(
+                    ['python', '-B', str(dis_test_file)],
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE
+                )
+                out, err = p.communicate()
 
-            p = subprocess.Popen(
-                ['python', '-B', str(dis_test_file)],
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE
-            )
-            out, err = p.communicate()
+                dis_test_file.remove()
 
-            dis_test_file.remove()
+                code = compile(file_content, '<string>', 'exec')
 
-            code = compile(file_content, '<string>', 'exec')
+                f = {'number': None}
+                exec code in f
+                number = f['number'] or NUMBER
+                group.append({
+                    'dis': out,
+                    'file_content': file_content,
+                    'number': number,
+                    'timeit_min': min(timeit.repeat(f['a'], number=number)),
+                })
+            group.sort(key=lambda x: x['timeit_min'])
 
-            f = {'number': None}
-            exec code in f
-            number = f['number'] or NUMBER
-            dic = {
-                'dis': out,
-                'file_content': file_content,
-                'number': number,
-                'timeit_min': min(timeit.repeat(f['a'], number=number)),
+            yield {
+                'n': Tx,
+                'span': 12 / len(group),
+                'cases': group,
             }
-            group.append(dic)
-        group.sort(key=lambda x: x['timeit_min'])
 
-    context = {
+    ts = template.stream({
         'plat_sys': platform.system(),
         'plat_rel': platform.release(),
         'sys_version': sys.version,
-        'tests': [
-            {
-                'n': k + 1,
-                'span': 12 / len(v),
-                'cases': v,
-            }
-            for k, v in enumerate(TESTS)
-        ]
-    }
-
-    print template.render(context)
+        'tests': gen_tests()
+    })
+    ts.enable_buffering()
+    ts.dump('index.html')
 
 
 if __name__ == '__main__':
